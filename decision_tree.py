660c1dc38bccb4bb23d0c4a4394e93dca98e1e03
PrathUpadhyay <despairdarkness@gmail.com>
2016-08-10 11:05:32 +0530

    Naive Bayes and Decision Tree

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-
-    
+  
 import sys
 from time import time
 import preprocess
@@ -11,27 +10,34 @@ import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, labels_train, features_test, labels_test = preprocess.load_data()
 
-
 dataset_size = len(features_train)
 features_train = features_train.reshape(dataset_size,-1)
 
 dataset_size_test = len(features_test)
 features_test = features_test.reshape(dataset_size_test,-1)
 
+
+
+
+
 #########################################################
 
-import numpy as np
-from sklearn.naive_bayes import GaussianNB
+
+from sklearn import tree
+
+print len(features_train), len(labels_train)
+
+clf = tree.DecisionTreeClassifier(min_samples_split=50)
+clf = clf.fit(features_train, labels_train)
+y_pred = clf.predict(features_test)
+
 from sklearn.metrics import accuracy_score
+print "Accuracy: ", accuracy_score(labels_test, y_pred)
 
-clf = GaussianNB()
-t0 = time()
-clf.fit(features_train, labels_train)
-print "training time:", round(time()-t0, 3), "s"
 
-t0 = time()
-predict = clf.predict(features_test)
-print "prediction time:", round(time()-t0, 3), "s"
-print "Accuracy score: ", accuracy_score(labels_test, predict)
+
+###best accuracy = 35%, for split= 50
 
 #########################################################
+
+
--- svm.py
+++ svm.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-    
 import sys
 from time import time
 import preprocess
@@ -11,27 +10,45 @@ import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, labels_train, features_test, labels_test = preprocess.load_data()
 
-
 dataset_size = len(features_train)
 features_train = features_train.reshape(dataset_size,-1)
 
 dataset_size_test = len(features_test)
 features_test = features_test.reshape(dataset_size_test,-1)
 
+
+
 #########################################################
 
-import numpy as np
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
 
-clf = GaussianNB()
+import numpy as np
+from sklearn.svm import SVC
+clf = SVC(C=1.0, kernel='rbf')
+
+#features_train = features_train[:len(features_train)/10]
+#labels_train = labels_train[:len(labels_train)/10]
+
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
 
 t0 = time()
-predict = clf.predict(features_test)
+pred = clf.predict(features_test)
 print "prediction time:", round(time()-t0, 3), "s"
-print "Accuracy score: ", accuracy_score(labels_test, predict)
+from sklearn.metrics import accuracy_score
+print "Accuracy Score: ", accuracy_score(labels_test,pred)
+
+#print "Values: ", sum(pred == 1)
+
+
+###best for C=1, and full dataset_size ===== 35%
+'''
+training time: 1464.42 s
+prediction time: 101.125 s
+Accuracy Score:  0.3537
+'''
+
 
 #########################################################
+
+
